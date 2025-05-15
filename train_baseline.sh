3a5237bcb9d35241995a448e38a2a3ce5afe4a2d
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-05-15 14:21:21 +0100

    bsz=16

--- train_baseline.sh
+++ train_baseline.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH=$(pwd)/src
-python src/hc_baseline/ex_qvim.py --batch_size 128 --tau_trainable --id hc_tc0
+python src/hc_baseline/ex_qvim.py --batch_size 16 --tau_trainable --id hc_tc0
 

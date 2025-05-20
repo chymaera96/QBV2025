d5886e9520f7df23cccfc761892a068dddc5dd4d
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-05-20 17:58:21 +0100

    hc_tc1: increasing n_epochs

--- train_baseline.sh
+++ train_baseline.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH=$(pwd)/src
-python src/hc_baseline/ex_qvim.py --batch_size 128 --precision=32 --id hc_tc1
+python src/hc_baseline/ex_qvim.py --batch_size 128 --precision=32 --n_epochs=40 --id hc_tc1
 

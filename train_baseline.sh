afe3174db265bd26cd01f77e52c796b28161abb6
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-05-21 15:12:12 +0100

    updated trainer bash

--- train_baseline.sh
+++ train_baseline.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=$(pwd)/src
 python src/qvim_mn_baseline/ex_qvim.py --num_gpus 1 --precision bf16-mixed \
                                         --batch_size 128 --acc_grad 1 \
-                                        --n_epochs 100 --tau_trainable \
-                                        --id dist_tc0
+                                        --n_epochs 100 \
+                                        --id dist_tc1
 

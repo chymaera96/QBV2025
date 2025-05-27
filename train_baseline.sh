9f758fc0cebcf8af6df0863a7f70c461ae26684f
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-05-27 15:40:51 +0100

    distil_tc3: shared projector

--- train_baseline.sh
+++ train_baseline.sh
@@ -2,5 +2,5 @@ export PYTHONPATH=$(pwd)/src
 python src/qvim_mn_baseline/ex_qvim.py --num_gpus 1 --precision bf16-mixed \
                                         --batch_size 128 --acc_grad 1 \
                                         --n_epochs 100 \
-                                        --id dist_tc2
+                                        --id dist_tc3
 

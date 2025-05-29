1020d75657b6b11427138ebdddbe2e8a10216f20
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-05-29 12:07:11 +0100

    passt_1: increased augmentations + dropout

--- train_baseline.sh
+++ train_baseline.sh
@@ -2,5 +2,5 @@ export PYTHONPATH=$(pwd)/src
 python src/qvim_mn_baseline/ex_qvim.py --num_gpus 1 --precision bf16-mixed \
                                         --batch_size 128 --acc_grad 1 \
                                         --n_epochs 100 \
-                                        --id passt_0
+                                        --id passt_1
 

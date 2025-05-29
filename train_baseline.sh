bf030470f6d3e65ed23040ef5ea93863cdc9d854
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-05-29 19:31:21 +0100

    passt_3

--- train_baseline.sh
+++ train_baseline.sh
@@ -3,5 +3,5 @@ python src/qvim_mn_baseline/ex_qvim.py --num_gpus 1 --precision bf16-mixed \
                                         --batch_size 64 --acc_grad 1 \
                                         --proj_dim 128
                                         --n_epochs 100 \
-                                        --id passt_2
+                                        --id passt_3
 

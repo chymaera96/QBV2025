73ba037416d7f74000ee333784f900a0d60aa405
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-05-29 17:28:22 +0100

    trying bsz=64

--- train_baseline.sh
+++ train_baseline.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=$(pwd)/src
 python src/qvim_mn_baseline/ex_qvim.py --num_gpus 1 --precision bf16-mixed \
-                                        --batch_size 128 --acc_grad 1 \
+                                        --batch_size 64 --acc_grad 1 \
                                         --n_epochs 100 \
                                         --id passt_2
 

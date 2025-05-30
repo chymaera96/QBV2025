1a5f20de826abbbe2f3ba6c598b8df7c5f866915
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-05-30 12:32:16 +0100

    no warmup passt_2

--- train_baseline.sh
+++ train_baseline.sh
@@ -3,5 +3,6 @@ python src/qvim_mn_baseline/ex_qvim.py --num_gpus 1 --precision bf16-mixed \
                                         --batch_size 64 --acc_grad 1 \
                                         --proj_dim 128 \
                                         --n_epochs 100 \
-                                        --id passt_2_epoch100
+                                        --warmup_steps 0 \
+                                        --id passt_2_2
 

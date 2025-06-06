238026824a503d1069e2926e528b204209a6f272
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-06-06 15:46:33 +0100

    SOTA baseline retake

--- train_baseline.sh
+++ train_baseline.sh
@@ -2,7 +2,6 @@ export PYTHONPATH=$(pwd)/src
 python src/qvim_mn_baseline/ex_qvim.py --num_gpus 1 --precision bf16-mixed \
                                         --batch_size 128 --acc_grad 1 \
                                         --proj_dim 128 \
-                                        --n_epochs 50 \
-                                        --warmup_steps 0 \
-                                        --id passt_2_3
+                                        --n_epochs 15 \
+                                        --id mn_2_0
 

8539e09924b7694579b78299d1f59b6d7072ca76
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-06-06 21:11:21 +0100

    minor fix

--- train_baseline.sh
+++ train_baseline.sh
@@ -4,7 +4,7 @@ export PYTHONPATH=$(pwd)/src
 #                                         --n_epochs 30 \
 #                                         --id mn_2_2
 
-python src/qvim_mn_baseline/pretrain.py --num_gpus 1 --precision bf16-mixed \
+python src/qvim_mn_baseline/pretrain.py --precision bf16-mixed \
                                         --batch_size 128 \
                                         --n_epochs 100 \
                                         --dataset_path /data/EECS-MachineListeningLab/datasets/FSD50K/dev_audio \

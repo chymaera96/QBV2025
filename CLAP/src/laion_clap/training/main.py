5d1eeb17c9f75902777a3072a5e2f0b77e449768
chrispla <cplachouras@gmail.com>
2025-06-07 21:23:28 +0000

    adjust by save freq

--- CLAP/src/laion_clap/training/main.py
+++ CLAP/src/laion_clap/training/main.py
@@ -594,14 +594,17 @@ def main():
                 continue
 
             if is_master(args):
-                if (
-                    (completed_epoch == args.epochs)
-                    or (args.save_frequency > 0 and completed_epoch)
-                    or (
-                        args.save_most_recent
-                        and (completed_epoch % args.zeroshot_frequency) == 0
-                    )
-                ):
+            if (
+                (completed_epoch == args.epochs)
+                or (args.save_frequency > 0 and (completed_epoch % args.save_frequency) == 0)
+                or (
+                    args.save_most_recent
+                    and (completed_epoch % args.zeroshot_frequency) == 0
+                )
+            ):
+                save_path = os.path.join(
+                    args.checkpoint_path, f"epoch_{completed_epoch}.pt"
+                )
                     save_path = os.path.join(
                         args.checkpoint_path, f"epoch_{completed_epoch}.pt"
                     )

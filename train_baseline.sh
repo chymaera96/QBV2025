539332709901df5cade75c84bc3fb8ec3ce887ab
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-06-15 17:51:13 +0100

    sht1: no augment

--- train_baseline.sh
+++ train_baseline.sh
@@ -10,4 +10,4 @@ export PYTHONPATH=$(pwd)/src
 python src/qvim_mn_baseline/ex_qvim.py --precision bf16-mixed \
                                         --batch_size 128 \
                                         --n_epochs 20 \
-                                        --id sht0
+                                        --id sht1

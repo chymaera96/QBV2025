6001837df1fed08eae9e19d2b59514f61bfd91a3
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-09-05 23:39:23 +0100

    minor fix

--- qvim_mbn_multi/ex_qvim.py
+++ qvim_mbn_multi/ex_qvim.py
@@ -47,32 +47,6 @@ class QVIMModule(pl.LightningModule):
             fmax_aug_range=config.fmax_aug_range
         )
 
-        # encoder = get_mobilenet(
-        #     width_mult=NAME_TO_WIDTH(config.pretrained_name),
-        #     pretrained_name=None
-        # )
-
-        # if config.pretrained_ckpt_path:
-        #     print(f"Loading pretrained encoder from {config.pretrained_ckpt_path}")
-        #     try:
-        #         ckpt = torch.load(config.pretrained_ckpt_path, map_location='cpu')
-        #         state_dict = ckpt['state_dict'] if 'state_dict' in ckpt else ckpt
-
-        #         # Strict loading to ensure all keys match
-        #         encoder.load_state_dict({
-        #             k.replace('encoder.', ''): v
-        #             for k, v in state_dict.items()
-        #             if k.startswith('encoder.')
-        #         }, strict=True)
-
-        #         print("Pretrained weights loaded successfully.")
-        #     except Exception as e:
-        #         print(f"Error loading checkpoint: {e}")
-        #         print(f"Current working directory: {os.getcwd()}")
-        #         raise RuntimeError("Failed to load pretrained weights. Exiting.")
-        # else:
-        #     raise ValueError("Pretrained checkpoint path not provided. Exiting.")
-
         self.encoder = get_mobilenet(
             width_mult=NAME_TO_WIDTH(config.pretrained_name),
             pretrained_name=config.pretrained_name,
